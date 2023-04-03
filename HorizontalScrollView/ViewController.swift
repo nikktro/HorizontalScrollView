c40099d272732bf01846fba0ca506805f994e454
Nikolay Trofimov <nikktro@gmail.com>
2023-04-03 20:39:05 +0300

    Initial Commit

---
+++ HorizontalScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorizontalScrollView
+//
+//  Created by Nikolay Trofimov on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
